7f922b400f6721bf11048630b4798545aece81db
unknown <tkasabuckij@gmail.com>
2020-03-02 12:36:55 +0300

    JavaScript code was refactored

--- main.js
+++ main.js
@@ -1,33 +1,26 @@
-function apiLoad() {
-    return fetch("https://api.openweathermap.org/data/2.5/weather?q=Gomel&appid=802a121e8d5faa4aecf1cde8bb779008")
-    .then(resp => resp.json()) // Convert data to json
+const cityDiv = document.getElementById('cityDiv');
+const timezone = document.getElementById('timezone');
+const mainInfomation = document.getElementById('mainInformation');
+const description = document.getElementById('weatherimgdescription');
+const activeTemp = document.getElementById('tempSelect');
+
+async function apiLoad() {
+    const resp = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Gomel&appid=802a121e8d5faa4aecf1cde8bb779008");
+    return await resp.json(); // Convert data to json
     //.then(data => console.log(data))
 }
 
+//Change Temp value
+activeTemp.addEventListener('change', () => {
+    apiLoad().then(data => convertTemp(data));
+});
+
 function showInformation(item) {
-    const cityDiv = document.getElementById('cityDiv');
-    const weatherDIV = document.getElementById('weatherDiv');
-    const mainInfomation = document.getElementById('mainInformation');
-    const description = document.getElementById('weatherimgdescription');
-
-    const activeTemp = document.getElementById('tempSelect');
-    activeTemp.addEventListener('change', change => {
-        if(activeTemp.value == 'cilcius') {
-            mainInfomation.innerText = Math.floor(cilcius) + "°C";
-        }
-        if(activeTemp.value == 'fahrenheit') {
-            mainInfomation.innerText = Math.floor(fahrenheit) + "°F";
-        }
-    });
-
-    //Convert from kelvin into cilcius
     let cilcius = JSON.stringify(item.main.temp) - 273;
-    let fahrenheit = (JSON.stringify(item.main.temp) - 273.15) * 9 / 5 + 32
-
     //City
     cityDiv.innerText = "Current city is " + item.name + ', ' + JSON.stringify(item.sys.country).replace(/\"/g, "");   
     //Timezone 
-    weatherDIV.innerText = "current timezone is " + item.timezone;
+    timezone.innerText = "current timezone is " + item.timezone;
     //Temputure
     mainInfomation.innerText = Math.floor(cilcius) + "°C";
     //Description
@@ -36,6 +29,17 @@ function showInformation(item) {
     console.log(item);
 }
 
+function convertTemp(item) {
+    //Convert from kelvin into cilcius
+    let cilcius = JSON.stringify(item.main.temp) - 273;
+    let fahrenheit = (JSON.stringify(item.main.temp) - 273.15) * 9 / 5 + 32;
 
+    if(activeTemp.value == 'cilcius') {
+        mainInfomation.innerText = Math.floor(cilcius) + "°C";
+    }
+    if(activeTemp.value == 'fahrenheit') {
+        mainInfomation.innerText = Math.floor(fahrenheit) + "°F";
+    }
+}
 
 apiLoad().then(data => showInformation(data));
