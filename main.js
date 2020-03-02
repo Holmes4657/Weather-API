00eb12a781f26b8c2bdaa88bb3cd39170c88896f
unknown <tkasabuckij@gmail.com>
2020-03-02 15:26:44 +0300

    Add scss

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const cityDiv = document.getElementById('cityDiv');
 const timezone = document.getElementById('timezone');
 const mainInfomation = document.getElementById('mainInformation');
-const description = document.getElementById('weatherimgdescription');
+const description = document.getElementById('imgDescription');
 const activeTemp = document.getElementById('tempSelect');
 
 async function apiLoad() {
     const resp = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Gomel&appid=802a121e8d5faa4aecf1cde8bb779008");
     return await resp.json(); // Convert data to json
-    //.then(data => console.log(data))
 }
 
 //Change Temp value
@@ -15,6 +14,7 @@ activeTemp.addEventListener('change', () => {
     apiLoad().then(data => convertTemp(data));
 });
 
+//Main Information
 function showInformation(item) {
     let cilcius = JSON.stringify(item.main.temp) - 273;
     //City
@@ -29,6 +29,7 @@ function showInformation(item) {
     console.log(item);
 }
 
+//Convert
 function convertTemp(item) {
     //Convert from kelvin into cilcius
     let cilcius = JSON.stringify(item.main.temp) - 273;
@@ -42,4 +43,19 @@ function convertTemp(item) {
     }
 }
 
+//Draw Image
+const showImageDiv = document.getElementsByClassName('card__weatherInfo-showImage')[0];
+
+function drawWeatherImage(item) {
+    const photo = 'http://openweathermap.org/img/w/' + item.weather[0].icon + '.png';
+    const weatherImage = `<img class="weatherImage" src="${photo}" alt="./images/stub.png">`
+    showImageDiv.innerHTML = weatherImage;
+}
+
+function drawWeatherImage1(showImageDiv) {
+    showImageDiv.innerHTML = '';
+    showImageDiv(element => drawWeatherImage(element));
+}
+
 apiLoad().then(data => showInformation(data));
+apiLoad().then(data => drawWeatherImage(data));
